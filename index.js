f6c861b93afcabc14995542862e71b7dca4f7147
Oleg Kudinov VPS <OlegK@VantagePointSoftware.com>
2019-02-01 16:07:02 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+(function () {
+    const button = document.querySelector('#find');
+    const input = document.querySelector('#search');
+    const resultdiv = document.querySelector('#result');
+    const calories = document.querySelector('#bycalories');
+
+    function buildHtml(data) {
+
+        let nutrients = '';
+        data.nutrients.forEach(element => {
+            nutrients += '<div><span class="nutrient">' + element.nutrient + ': </span><strong>' + element.content.value + ' ' + element.content.unit + '</strong></div>';
+        });
+
+        let result =
+            '<div>' +
+            "<div><strong>" + data.foodName + "</strong></div>" +
+            "<div><em>Weight:</em><strong> " + data.weight.value + ' ' + data.weight.unit + "</strong></div>" +
+            "<div><em>Energy:</em><strong> "  + data.energy.value + ' ' + data.energy.unit + "</strong></div>" +
+            "<p>" + nutrients + "</p>";
+            '</div>';
+        return result;
+    }
+
+    const request = new XMLHttpRequest();
+    request.onload = function (obj) {
+        var data = request.response;
+        var html = buildHtml(data);
+        resultdiv.innerHTML = html;
+        console.log(obj)
+    }
+
+    request.onerror = function (obj) {
+
+        console.log(obj)
+    }
+
+    button.addEventListener('click', function () {
+        let command = 'w';
+        if (calories.checked)
+            command = 'c';
+        let search = command + ',' + input.value;
+
+        let requestURL = 'https://oktstdevusda.azurewebsites.net/api/nutrients/' + search;
+        //let requestURL = 'https://localhost:44392/api/nutrients/' + search;
+        request.open('GET', requestURL);
+        request.responseType = 'json';
+        request.send();
+    });
+})();
