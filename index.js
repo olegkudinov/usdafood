df3bad85401a1278e87c75246d783e14ab1a804c
Oleg Kudinov <oleg.kudinov@live.com>
2019-02-09 16:09:03 -0500

    adding history handling

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-const button = document.querySelector('#find');
-const input = document.querySelector('#search');
-const resultdiv = document.querySelector('#result');
-const calories = document.querySelector('#bycalories');
-const spinner = document.getElementsByClassName("spinner")[0];
-const btncaption = document.getElementById("btncaption");
+let app = (function (window) {
+    const button = document.querySelector('#find');
+    const input = document.querySelector('#search');
+    const resultdiv = document.querySelector('#result');
+    const calories = document.querySelector('#bycalories');
+    const spinner = document.getElementsByClassName("spinner")[0];
+    const btncaption = document.getElementById("btncaption");
 
-//const _baseUrl = 'https://localhost:44392';
-const _baseUrl = 'https://oktstdevusda.azurewebsites.net';
+    //const _baseUrl = 'https://localhost:44392';
+    const _baseUrl = 'https://oktstdevusda.azurewebsites.net';
 
-spinner.className = 'invisible';
+    spinner.className = 'invisible';
 
-function buildNutientHtml(data, bycalories) {
+    function buildNutientHtml(data, bycalories) {
 
-    let nutrients = '';
-    data.nutrients.forEach(element => {
-        let header = element.nutrient[0] === "*";
-        let unitless = element.content.unit === "header";
-        let row = '';
-        if (header && unitless) {
-            row = '<tr><td colspan="3"><strong>' + element.nutrient + '</strong></td></tr>'
-        } else if (header) {
-            row = '<tr><td><strong>' + element.nutrient + '</strong></td><td><strong>' + element.content.value + '</strong></td><td><strong>' + element.content.unit + '</strong></td></tr>';
-        } else {
-            row = '<tr><td><em>' + element.nutrient + '</em></td><td><strong>' + element.content.value + '</strong></td><td><strong>' + element.content.unit + '</strong></td></tr>';
-        }
-        nutrients += row;
-    });
+        let nutrients = '';
+        data.nutrients.forEach(element => {
+            let header = element.nutrient[0] === "*";
+            let unitless = element.content.unit === "header";
+            let row = '';
+            if (header && unitless) {
+                row = '<tr><td colspan="3"><strong>' + element.nutrient + '</strong></td></tr>'
+            } else if (header) {
+                row = '<tr><td><strong>' + element.nutrient + '</strong></td><td><strong>' + element.content.value + '</strong></td><td><strong>' + element.content.unit + '</strong></td></tr>';
+            } else {
+                row = '<tr><td><em>' + element.nutrient + '</em></td><td><strong>' + element.content.value + '</strong></td><td><strong>' + element.content.unit + '</strong></td></tr>';
+            }
+            nutrients += row;
+        });
 
-    let totals = `
+        let totals = `
         <div><em>Weight:</em><strong> ` + data.weight.value + ` ` + data.weight.unit + `</strong></div>
         <div><em>Energy:</em><strong> ` + data.energy.value + ` ` + data.energy.unit + `</strong></div>`;
-    
-    if(bycalories) {
-        totals = `
+
+        if (bycalories) {
+            totals = `
         <div><em>Energy:</em><strong> ` + data.energy.value + ` ` + data.energy.unit + `</strong></div>
         <div><em>Weight:</em><strong> ` + data.weight.value + ` ` + data.weight.unit + `</strong></div>`;
-    }
+        }
 
-    let result = `
-        <div><strong>` + data.foodName + `</strong></div>` 
-        + totals + `
+        let result = `
+        <div><strong>` + data.foodName + `</strong></div>`
+            + totals + `
         <table>
             <thead>
                 <th>Nutrition</th>
@@ -50,97 +51,110 @@ function buildNutientHtml(data, bycalories) {
             nutrients + `
             </tbody>
         </table>`;
-    return result;
-}
+        return result;
+    }
 
-function buildFoodHtml(data) {
-    let foodHtml = '';
-    data.forEach(element => {
-        let id = element.id
-        foodHtml += `
+    function buildFoodHtml(data) {
+        let foodHtml = '';
+        data.forEach(element => {
+            let id = element.id
+            foodHtml += `
                 <tr>
                     <td>` + element.name + `</td>
-                    <td><button onclick='getNutrientsFor("` + id  + `")'>Nutrients</button></td>
+                    <td><button onclick='app.getNutrientsFor("` + id + `")'>Nutrients</button></td>
                 </tr>`;
-    });
-    let result = `
+        });
+        let result = `
         <table>
             <thead>
                 <th></th>
                 <th></th>
             </thead>
             <tbody>` +
-        foodHtml + `
+            foodHtml + `
             </tbody>
         </table>`;
-    return result;
-}
-
-const foodRequest = new XMLHttpRequest();
-
-foodRequest.onload = function (obj) {
-    btncaption.innerText = "Find";
-    spinner.className = 'invisible';
-    let data = foodRequest.response;
-    if (!data || data === null || data.length === 0) {
-        resultdiv.innerHTML = "<div><strong>Not Found<strong></div>";
-    } else {
-        var html = buildFoodHtml(data);
-        resultdiv.innerHTML = html;
+        return result;
     }
-    console.log(obj)
-}
 
-foodRequest.onerror = function (obj) {
-    btncaption.innerText = "Find";
-    spinner.className = 'invisible';
-    resultdiv.innerHTML = "<div><strong>Something's wrong<strong></div>";
-    console.log(obj)
-}
+    const foodRequest = new XMLHttpRequest();
 
-const nutrientRequest = new XMLHttpRequest();
-
-nutrientRequest.onload = function (obj) {
-    btncaption.innerText = "Back";
-    spinner.className = 'invisible';
-    let data = nutrientRequest.response;
-    if (!data || data === null) {
-        resultdiv.innerHTML = "<div><strong>Not Found<strong></div>";
-    } else {
-        var html = buildNutientHtml(data, calories.checked);
-        resultdiv.innerHTML = html;
+    foodRequest.onload = function (obj) {
+        btncaption.innerText = "Find";
+        spinner.className = 'invisible';
+        let data = foodRequest.response;
+        if (!data || data === null || data.length === 0) {
+            resultdiv.innerHTML = "<div><strong>Not Found<strong></div>";
+        } else {
+            var html = buildFoodHtml(data);
+            resultdiv.innerHTML = html;
+        }
+        console.log(obj)
     }
-    console.log(obj)
-}
 
-nutrientRequest.onerror = function (obj) {
-    btncaption.innerHTML = "Back";
-    spinner.className = 'invisible';
-    resultdiv.innerHTML = "<div><strong>Something's wrong<strong></div>";
-    console.log(obj)
-}
-
-function startRequest(request, url) {
-    request.open('GET', url);
-    request.responseType = 'json';
-    request.send();
-    spinner.className = '';
-}
-
-button.addEventListener('click', function () {
-    startRequest(foodRequest, _baseUrl + '/api/foods/' + input.value);
-});
-
-input.addEventListener("keyup", function (event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        button.click();
+    foodRequest.onerror = function (obj) {
+        btncaption.innerText = "Find";
+        spinner.className = 'invisible';
+        resultdiv.innerHTML = "<div><strong>Something's wrong<strong></div>";
+        console.log(obj)
     }
-});
 
-function getNutrientsFor(id) {
-    let command = 'w';
-    if (calories.checked)
-        command = 'c';
-    startRequest(nutrientRequest, _baseUrl + '/api/nutrients/' + command + ',' + id);
-}
+    const nutrientRequest = new XMLHttpRequest();
+
+    nutrientRequest.onload = function (obj) {
+        btncaption.innerText = "Back";
+        spinner.className = 'invisible';
+        let data = nutrientRequest.response;
+        if (!data || data === null) {
+            resultdiv.innerHTML = "<div><strong>Not Found<strong></div>";
+        } else {
+            var html = buildNutientHtml(data, calories.checked);
+            resultdiv.innerHTML = html;
+        }
+        console.log(obj)
+    }
+
+    nutrientRequest.onerror = function (obj) {
+        btncaption.innerHTML = "Back";
+        spinner.className = 'invisible';
+        resultdiv.innerHTML = "<div><strong>Something's wrong<strong></div>";
+        console.log(obj)
+    }
+
+    function startRequest(requestKind, url, push) {
+        if (push)
+            window.history.pushState({ Url: url, Request: requestKind }, "search", url);
+        let request = requestKind === 'nutrient' ? nutrientRequest : foodRequest;
+        request.open('GET', _baseUrl + url);
+        request.responseType = 'json';
+        request.send();
+        spinner.className = '';
+    }
+
+    window.onpopstate = function (evt) {
+        if (evt.state)
+            startRequest(evt.state.Request, evt.state.Url, false);
+    };
+
+    button.addEventListener('click', function () {
+        startRequest('food', '/api/foods/' + input.value, true);
+    });
+
+    input.addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            button.click();
+        }
+    });
+
+    _getNutrientsFor = function (id) {
+        let command = 'w';
+        if (calories.checked)
+            command = 'c';
+        startRequest('nutrient', '/api/nutrients/' + command + ',' + id, true);
+    };
+
+    return {
+        getNutrientsFor: _getNutrientsFor,
+    }
+}(this));
