4876da9677855fbb48b52d838dc18df841a2ae0f
Oleg Kudinov <oleg.kudinov@live.com>
2019-03-02 14:54:25 -0500

    ui changes

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ let app = (function (window) {
     const button = document.querySelector('#find');
     const input = document.querySelector('#search');
     const resultdiv = document.querySelector('#result');
-    const calories = document.querySelector('#bycalories');
     const spinner = document.getElementsByClassName("spinner")[0];
     const btncaption = document.getElementById("btncaption");
-    const multiplier = document.querySelector('#multiplier');
 
     //const _baseUrl = 'https://localhost:44392';
     const _baseUrl = 'https://oktstdevusda.azurewebsites.net';
     let _startUrl = window.location.href;
 
-    let _factor = 1;
+    let _mfactor = 1;
     let _lastResult = null;
+    let _byCalories = false;
+    let _factorCaption = "x " + _mfactor;
+    let _baseCation = _byCalories ? "By Calories" : "By Weight";
 
     spinner.className = 'invisible';
 
-    function buildNutientHtml(data, bycalories) {
+    function buildNutientHtml(data) {
 
+        let factor = (_byCalories ? 100.0 / data.energy.value : 1.0) * _mfactor;
         let nutrients = '';
         _lastResult = data;
         data.nutrients.forEach(element => {
@@ -25,27 +27,31 @@ let app = (function (window) {
             let unitless = element.content.unit === "header";
             let row = '';
             if (header && unitless) {
-                row = '<tr><td colspan="3"><strong>' + element.nutrient + '</strong></td></tr>'
+                row = '<tr><td colspan="3"><strong>' + element.nutrient.replace('**', '').replace('**', '') + '</strong></td></tr>'
             } else if (header) {
-                row = '<tr><td><strong>' + element.nutrient + '</strong></td><td><strong>' + element.content.value * _factor + '</strong></td><td><strong>' + element.content.unit + '</strong></td></tr>';
+                row = '<tr><td><strong>' + element.nutrient.replace('**', '').replace('**', '') + '</strong></td><td><strong>' + Math.round(element.content.value * factor) + '</strong></td><td><strong>' + element.content.unit + '</strong></td></tr>';
             } else {
-                row = '<tr><td><em>' + element.nutrient + '</em></td><td><strong>' + element.content.value * _factor + '</strong></td><td><strong>' + element.content.unit + '</strong></td></tr>';
+                row = '<tr><td><em>' + element.nutrient + '</em></td><td><strong>' + Math.round(element.content.value * factor) + '</strong></td><td><strong>' + element.content.unit + '</strong></td></tr>';
             }
             nutrients += row;
         });
 
         let totals = `
-        <div><em>Weight:</em><strong> ` + data.weight.value * _factor + ` ` + data.weight.unit + `</strong></div>
-        <div><em>Energy:</em><strong> ` + data.energy.value * _factor + ` ` + data.energy.unit + `</strong></div>`;
+        <div><em>Weight:</em><strong> ` + Math.round(data.weight.value * factor) + ` ` + data.weight.unit + `</strong></div>
+        <div><em>Energy:</em><strong> ` + Math.round(data.energy.value * factor) + ` ` + data.energy.unit + `</strong></div>`;
 
-        if (bycalories) {
+        if (_byCalories) {
             totals = `
-        <div><em>Energy:</em><strong> ` + data.energy.value * _factor + ` ` + data.energy.unit + `</strong></div>
-        <div><em>Weight:</em><strong> ` + data.weight.value * _factor + ` ` + data.weight.unit + `</strong></div>`;
+        <div><em>Energy:</em><strong> ` + Math.round(data.energy.value * factor) + ` ` + data.energy.unit + `</strong></div>
+        <div><em>Weight:</em><strong> ` + Math.round(data.weight.value * factor) + ` ` + data.weight.unit + `</strong></div>`;
         }
 
-        let result = `
-        <div><strong>` + data.foodName + `</strong></div>`
+        const result = `
+        <div>
+        <strong>` + data.foodName + `&nbsp;&nbsp;&nbsp;</strong>
+        <button id='toggler' onclick='app.toggleBase()'>` + _baseCation + `</button>
+        <button id='multiplier' onclick='app.multiply()'>` + _factorCaption + `</button>
+        </div>`
             + totals + `
         <table>
             <thead>
@@ -108,20 +114,18 @@ let app = (function (window) {
     const nutrientRequest = new XMLHttpRequest();
 
     nutrientRequest.onload = function (obj) {
-        btncaption.innerText = "Back";
         spinner.className = 'invisible';
         let data = nutrientRequest.response;
         if (!data || data === null) {
             resultdiv.innerHTML = "<div><strong>Not Found<strong></div>";
         } else {
-            var html = buildNutientHtml(data, calories.checked);
+            var html = buildNutientHtml(data);
             resultdiv.innerHTML = html;
         }
         console.log(obj)
     }
 
     nutrientRequest.onerror = function (obj) {
-        btncaption.innerHTML = "Back";
         spinner.className = 'invisible';
         resultdiv.innerHTML = "<div><strong>Something's wrong<strong></div>";
         console.log(obj)
@@ -160,31 +164,32 @@ let app = (function (window) {
         }
     });
 
-    multiplier.addEventListener('click', function () {
-        if(++_factor > 10)
-            _factor = 1;
-        multiplier.innerText = "x " + _factor;
+    _multiply = function () {
+        if(++_mfactor > 10)
+            _mfactor = 1;
+        _factorCaption = "x " + _mfactor;
         if(_lastResult) {
-            var html = buildNutientHtml(_lastResult, calories.checked);
+            var html = buildNutientHtml(_lastResult);
             resultdiv.innerHTML = html;
         }
-    });
+    };
 
-    calories.addEventListener('click', function () {
+    _toggleBase = function () {
+        _byCalories = !_byCalories;
+        _baseCation = _byCalories ? "By Weight" : "By Calories";
         if(_lastResult) {
-            var html = buildNutientHtml(_lastResult, calories.checked);
+            var html = buildNutientHtml(_lastResult);
             resultdiv.innerHTML = html;
         }
-    });
+    };
 
     _getNutrientsFor = function (id) {
-        let command = 'w';
-        if (calories.checked)
-            command = 'c';
-        startRequest('/api/nutrients/' + command + ',' + id, true);
+        startRequest('/api/nutrients/w,' + id, true);
     };
 
     return {
         getNutrientsFor: _getNutrientsFor,
+        multiply: _multiply,
+        toggleBase: _toggleBase
     }
 }(this));
