7fe50c599b10c507c88e0612a7844e249304c7c1
Oleg Kudinov VPS <OlegK@VantagePointSoftware.com>
2019-11-21 16:50:02 -0500

    moving install button to different place

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('usdafood/sw.js')
+    navigator.serviceWorker.register('/usdafood/sw.js')
         .then(reg => console.log('sw is registered', reg))
         .catch(err => console.log('sw registration failed', err));
 }
